e8cbf63f53a5bdd7c6183a7d4d9b8a0ed01e900a
KID <50666207+KID421@users.noreply.github.com>
2019-07-17 16:33:46 +0800

    Player 基本控制

---
+++ PhotonProject/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    public class Player : MonoBehaviour
+    {
+        public Rigidbody2D rig;
+        public float speed = 10;
+
+        /// <summary>
+        /// 固定頻率執行事件
+        /// </summary>
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            // 剛體.推力(三維向量)
+            // 角色.右邊 * ("Horizontal" - A D)
+            // 角色.上方 * ("Vertical" - W S)
+            rig.AddForce((
+                transform.right * Input.GetAxisRaw("Horizontal") +
+                transform.up * Input.GetAxisRaw("Vertical")) * speed);
+        }
+    }
+}
